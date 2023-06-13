5da12e8cbf40e5eebf693602889011f508afae9c
Sandeep Kumar <47131131+sandpkr@users.noreply.github.com>
2023-06-13 11:46:28 -0400

    Added week 2 materials

---
+++ Week_2/example_3_dim_matrix.m
@@ -0,0 +1,12 @@
+%%%% This file will show you an example of a 3d matrix
+clear
+clc
+close all
+
+load mristack.mat  %%% this is an example data provided by MATLAB
+
+for i=1:21
+    imagesc(mristack(:,:,i))
+    title(num2str(i))
+    pause(1)        %%% This wil pause the loop execution by 1 sec
+end
