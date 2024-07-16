344e30a494fe48e913e6839b82fda20b8f4b0b44
Sandeep Kumar <47131131+sandpkr@users.noreply.github.com>
2024-07-16 12:49:25 -0400

    Added materials for week 7

---
+++ summer_2024/week_7/find_peaks_on_stim_and_velocity_data_1.m
@@ -0,0 +1,33 @@
+clear
+clc
+close all
+
+load('velocity_array.mat') %load velocity data
+load('stimulus_array.mat') %load stimulus data
+
+% for i=1:size(stimulus_array,1)
+%     test_array=stimulus_array(i,:);
+%     plot(test_array) % where ever you see a peak, stimulus was delivered at that instant
+%     title(num2str(i)) % adding the title, so we know which iteration it is
+%     waitforbuttonpress % for convinience. this will execute loop only when we press a button
+%     close all % deleting the plot, so that we do not have so many figures 
+% end
+
+%% determining when the stimulus was delivered
+
+locs_array=[];  % define an empty array
+for i=1:size(stimulus_array,1)
+    test_array=stimulus_array(i,:);
+    [pks,locs] = findpeaks(test_array);
+    locs_array(i,:)=locs;
+end
+
+%% find the velocity at stimulus onset
+velocity_at_stim_onset_array=[];  % define an empty array
+for i=1:size(velocity_array,1)
+    test_array=velocity_array(i,:);
+    velocity_at_stim_onset_array(i,:)=test_array(locs_array(i));
+end
+
+%% plot histogram of velocity at stim onset
+histogram(velocity_at_stim_onset_array)
