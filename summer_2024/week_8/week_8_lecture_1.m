fa7319f6710b463d1d62d37e7b422f22e4ca69e6
Sandeep Kumar <47131131+sandpkr@users.noreply.github.com>
2024-07-30 15:00:17 -0400

    new changes

--- summer_2024/week_8/week_8_lecture_1.m
+++ summer_2024/week_8/week_8_lecture_1.m
@@ -3,14 +3,17 @@ clc
 close all
 
 rng(1);
-simulated_data = [randn(100,2) + 2; randn(100,2) - 2; randn(100,2) + [5, -5]];
+simulated_data = [randn(100,3) + 2; randn(100,3) - 2; randn(100,3) + 5];
+
+% scatter3(simulated_data(:,1), simulated_data(:,2), simulated_data(:,3));
+color_name=[1 0 0; 0 1 0; 0 0 1];
 
 num_clusters = 3;
 
 [idx, C] = kmeans(simulated_data, num_clusters);
 
 figure;
-gscatter(simulated_data(:,1), simulated_data(:,2), idx);
+scatter3(simulated_data(:,1), simulated_data(:,2), simulated_data(:,3));
 % scatter(simulated_data(:,1), simulated_data(:,2));
 hold on;
 plot(C(:,1), C(:,2), 'kx', 'MarkerSize', 15, 'LineWidth', 3);
